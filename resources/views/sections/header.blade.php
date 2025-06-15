9baaac90fde1df8d739d2c6f8def7e67c7185133
Xolof <olof@liberdev.se>
2025-06-15 08:53:26 +0200

    Add some styles to the wp_nav_menu

--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -5,7 +5,12 @@
 
   @if (has_nav_menu('primary_navigation'))
     <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
-      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
+      {!! wp_nav_menu([
+        'theme_location' => 'primary_navigation',
+        'menu_class' => 'nav flex flex-row mb-4',
+        'echo' => false,
+        'a_class_0' => "font-bold inline-flex items-center mr-6",
+        ]) !!}
     </nav>
   @endif
 </header>
