7b39e5cfe788fdd02bc1fec0e685128b9707873a
Xolof <olof@liberdev.se>
2025-06-16 15:43:04 +0200

    break up attributes in mobile menu into several lines

--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -13,7 +13,11 @@
         </button>
 
         <!-- Mobile Menu -->
-        <div x-show="mobileMenuIsOpen" @click.away="mobileMenuIsOpen = false" class="fixed inset-0 md:hidden transition-transform duration-300 bg-zinc-50 z-900" :class="{ 'translate-x-0': mobileMenuIsOpen, '-translate-x-full': !mobileMenuIsOpen }">
+        <div x-show="mobileMenuIsOpen"
+          @click.away="mobileMenuIsOpen = false"
+          class="fixed inset-0 md:hidden transition-transform duration-300 bg-zinc-50 z-900"
+          :class="{ 'translate-x-0': mobileMenuIsOpen, '-translate-x-full': !mobileMenuIsOpen }"
+        >
             <div class="flex justify-between p-4">
                 <div class="text-zinc-800 text-xl">
                     <a href="/" class="brand block">{!! $siteName !!}</a>
